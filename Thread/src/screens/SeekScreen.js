46b6bf8fab8e2559290fb224f459076c8bebe142
Jihyeon Janel Lee <janel@cs.stanford.edu>
2019-11-14 23:12:04 -0800

    app with profile

---
+++ Thread/src/screens/SeekScreen.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { LayoutAnimation, RefreshControl } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class App extends React.Component {
+render() {
+    		return (
+      		<View style={styles.container}>
+        			<Text>Seek!</Text>
+      		</View>
+    		);
+  	}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
